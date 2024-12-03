1770cc08782fdb53545f04f8e318d23d3ccfd389
Tianjiahe-isep <jiti63893@eleve.isep.fr>
2024-12-03 21:19:16 +0100

    Add Employe.java class

--- src/main/java/fr/isep/ji/Employe.java
+++ src/main/java/fr/isep/ji/Employe.java
@@ -1,4 +1,8 @@
 package fr.isep.ji;
 
-public class Employe {
+public class Employe extends Personne{
+
+    public Employe(int identifiant, String nom, String adresse, String contact) {
+        super(identifiant, nom, adresse, contact);
+    }
 }
