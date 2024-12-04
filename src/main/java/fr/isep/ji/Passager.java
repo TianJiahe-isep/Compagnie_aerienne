dad8e31258156c5cf71731bc8e3845fa3fe1111a
Tianjiahe-isep <jiti63893@eleve.isep.fr>
2024-12-04 16:11:17 +0100

    Commit Message

--- src/main/java/fr/isep/ji/Passager.java
+++ src/main/java/fr/isep/ji/Passager.java
@@ -1,17 +1,30 @@
 package fr.isep.ji;
 
 public class Passager extends Personne{
-    private int passport;
+    private String passeport;
+
+    public Passager(int identifiant, String nom, String adresse, String contact) {
+        super(identifiant, nom, adresse, contact);
+        this.passeport = passeport;
+    }
+
+    public String getPasseport() {
+        return passeport;
+    }
+
+    public void setPasseport(String passeport) {
+        this.passeport = passeport;
+    }
 
     public void reserverVol(){
-
+        System.out.println("Vol réservé pour le passager.");
     }
 
     public void annulerResercation(){
-
+        System.out.println("Réservation annulée pour le passager.");
     }
 
     public void obtenirReservation(){
-
+        System.out.println("Liste des réservations pour le passager.");
     }
 }
