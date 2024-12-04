4ade4e7dedaf6410c31ef6b0d7756ca98602933f
Tianjiahe-isep <jiti63893@eleve.isep.fr>
2024-12-04 18:56:03 +0100

    Add Vol.java class

--- src/main/java/fr/isep/ji/Reservation.java
+++ src/main/java/fr/isep/ji/Reservation.java
@@ -1,4 +1,44 @@
 package fr.isep.ji;
 
 public class Reservation {
+
+    private int numeroReservation;
+    private String dataReservation;
+    private String statu;
+
+    public void confirmerReservation(){
+
+    }
+
+    public void annulerReservation(){
+
+    }
+
+    public void modifierRservation(){
+
+    }
+
+    public int getNumeroReservation() {
+        return numeroReservation;
+    }
+
+    public void setNumeroReservation(int numeroReservation) {
+        this.numeroReservation = numeroReservation;
+    }
+
+    public String getDataReservation() {
+        return dataReservation;
+    }
+
+    public void setDataReservation(String dataReservation) {
+        this.dataReservation = dataReservation;
+    }
+
+    public String getStatu() {
+        return statu;
+    }
+
+    public void setStatu(String statu) {
+        this.statu = statu;
+    }
 }
